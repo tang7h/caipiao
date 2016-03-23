4dd1adbb0643c7b8cd72842ccfcdccce23e274a2
Ruifeng Tang <tang7h@gmail.com>
2016-03-23 10:57:05 +0800

    改进了对 Bootstrap4 的兼容

--- index.php
+++ index.php
@@ -7,15 +7,14 @@
 	<meta name="apple-mobile-web-app-status-bar-style" content="black">
 	<meta name="format-detection" content="telephone=no">
 	<meta name="Keywords" content="彩票,体育彩票,足球彩票,手机彩票,wap彩票,手机预定彩票"/>
-	<link rel="stylesheet" type="text/css" href="css/caipiao.css">
 	<title>彩票</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-	<script src="holder.js"></script>
-	<script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
 	<!-- Bootstrap -->
-	<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
-	<script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="css/caipiao.css">
+	<link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
+	<script src="https://cdn.bootcss.com/jquery/2.1.4/jquery.min.js"></script>
+	<script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="css/caipiao.css">
+	<script src="holder.js"></script>
 </head>
 <body>
 	<!-- 顶部轮播 -->
@@ -23,15 +22,15 @@
 		<div id="carousel-top" class="carousel slide" data-ride="carousel" data-interval="3000">
 			<!-- Carousel items -->
 			<div class="carousel-inner">
-				<div class="active item">carousel 1</div>
-				<div class="item">carousel 2</div>
-				<div class="item">carousel 3</div>
+				<div class="active carousel-item">carousel 1</div>
+				<div class="carousel-item">carousel 2</div>
+				<div class="carousel-item">carousel 3</div>
 			</div>
 		</div>
 	</div>
 
 	<!-- 快捷入口图标 -->
-	<div class="section-nav-icons card">
+	<div class="section-nav-icons section">
 		<a href="#" class="nav-item">
 			<div class="nav-item-icon"></div>
 			<div class="nav-item-label">链接</div>
@@ -55,7 +54,7 @@
 	</div>
 
 	<!-- 滚动新闻 -->
-	<div class="section-scroll-news card">
+	<div class="section-scroll-news section">
 		<div class="scroll-news">
 			<ul class="scroll-news-list">
 				<li class="scroll-news-item">
@@ -83,7 +82,7 @@
 	</div>
 
 	<!-- 文章列表 -->
-	<div class="article-list card card-with-margin">
+	<div class="article-list section with-padding with-margin">
 		<div class="article-item">
 			<img src="holder.js/120x80" alt="">
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias quasi sit, cumque</p>
