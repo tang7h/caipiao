f989129ff49b88bc23140abd19e52080e16d966d
Ruifeng Tang <tang7h@gmail.com>
2016-03-23 14:36:37 +0800

    从服务器同步文件

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+	header("Content-Type:text/html;charset=utf-8");//定义读取文本为utf-8；
+	require("config.php");//因为属于长时间链接数据库~所以使用require而不是include；
+	$sql = "SELECT title,pic from banner_img ORDER BY id limit 4";
+	$result = mysql_query($sql); //执行查询语句	//输出查询结果
+	while ($row = mysql_fetch_array($result)) {
+	//echo $row['title'] ,"<br />", $row['pic'],"<br />"; 
+		// foreach ($result as $val) {
+		// 	print_r($val);
+		// }
+		$arr[$i] = $row;
+		$i++;
+		}
+		
+		//  echo "<pre>";
+		//  print_r($arr);
+		// die;
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,14 +27,15 @@
 	<meta name="apple-mobile-web-app-status-bar-style" content="black">
 	<meta name="format-detection" content="telephone=no">
 	<meta name="Keywords" content="彩票,体育彩票,足球彩票,手机彩票,wap彩票,手机预定彩票"/>
-	<title>彩票</title>
-	<!-- Bootstrap -->
-	<link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
-	<script src="https://cdn.bootcss.com/jquery/2.1.4/jquery.min.js"></script>
-	<script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js"></script>
-
 	<link rel="stylesheet" type="text/css" href="css/caipiao.css">
+	<title>彩票</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<script src="holder.js"></script>
+	<script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/caipiao.css">
 </head>
 <body>
 	<!-- 顶部轮播 -->
@@ -22,15 +43,31 @@
 		<div id="carousel-top" class="carousel slide" data-ride="carousel" data-interval="3000">
 			<!-- Carousel items -->
 			<div class="carousel-inner">
-				<div class="active carousel-item">carousel 1</div>
-				<div class="carousel-item">carousel 2</div>
-				<div class="carousel-item">carousel 3</div>
+				<div class="active item">
+					<a href="">
+						<img src="<?php echo ''.$arr[1]['pic'].'';?>">
+						<p><?php echo ''.$arr[1]['title'].'';?></p>
+					</a>
+				</div>
+				<div class="item">
+					<a href="">
+						<img src="<?php echo ''.$arr[2]['pic'].'';?>">
+						<p><?php echo ''.$arr[2]['title'].'';?></p>
+					</a>
+				</div>
+				<div class="item">
+					<a href="">
+						<img src="<?php echo ''.$arr[3]['pic'].'';?>">
+						<p><?php echo ''.$arr[3]['title'].'';?></p>
+					</a>
+				</div>
+
 			</div>
 		</div>
 	</div>
 
 	<!-- 快捷入口图标 -->
-	<div class="section-nav-icons section">
+	<div class="section-nav-icons card">
 		<a href="#" class="nav-item">
 			<div class="nav-item-icon"></div>
 			<div class="nav-item-label">链接</div>
@@ -54,7 +91,7 @@
 	</div>
 
 	<!-- 滚动新闻 -->
-	<div class="section-scroll-news section">
+	<div class="section-scroll-news card">
 		<div class="scroll-news">
 			<ul class="scroll-news-list">
 				<li class="scroll-news-item">
@@ -82,7 +119,7 @@
 	</div>
 
 	<!-- 文章列表 -->
-	<div class="article-list section with-padding with-margin">
+	<div class="article-list card card-with-margin">
 		<div class="article-item">
 			<img src="holder.js/120x80" alt="">
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias quasi sit, cumque</p>
@@ -99,60 +136,4 @@
 
 	<!-- 底部导航和 floating action button -->
 	<div class="section-bottom">
-		<div class="nav-bottom">
-			<div class="nav-bottom-item">
-				<div class="icon">
-					<img src="images/iconfont-clock-03.png">
-				</div>
-				<div class="icon-label">
-					首页
-				</div>
-			</div>
-			
-			<div class="nav-bottom-item">
-				<a href="hunhe_spf.php">
-				<div class="icon">
-					<img src="images/iconfont-clock-04.png">
-				</div>
-				<div class="icon-label">
-					tab
-				</div>
-				</a>
-			</div>
-			
-			<div class="nav-bottom-item">
-				<div class="icon">
-					<img src="images/iconfont-clock-07.png">
-				</div>
-				<div class="icon-label">
-					tab
-				</div>
-			</div>
-			<div class="nav-bottom-item">
-				<div class="icon">
-					<img src="images/iconfont-clock-06.png">
-				</div>
-				<div class="icon-label">
-					tab
-				</div>
-			</div>
-			
-			<div class="nav-bottom-item">
-				<a href="user.php">
-				<div class="icon">
-					<img src="images/iconfont-clock-05.png">
-				</div>
-				<div class="icon-label">
-					tab
-				</div>
-				</a>
-			</div>
-			
-		</div>
-
-		<div class="fab">
-			<i class="fab-add"></i>
-		</div>
-	</div>
-</body>
-</html>
+		<div class="nav-bottom"
