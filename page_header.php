fdef7c621e0c6b0b0a62d5de0e5a28d1634f7aff
Ruifeng Tang <tang7h@gmail.com>
2016-03-28 14:52:52 +0800

    大幅改进底部导航性能

--- page_header.php
+++ page_header.php
@@ -2,55 +2,49 @@
   <div class="section-bottom">
     <div class="nav-bottom" id="nav-item-1">
 
-      <input type="radio" name="name" class="nav-toggle" id="item-1" checked>
-      <label class="nav-bottom-item" for="item-1" onclick="goto('index.php')">
+      <label class="nav-bottom-item active" for="item-1" data-url="index.php">
         <i class="material-icons">free_breakfast</i>
         <span class="nav-bottom-text">首页</span>
       </label>
 
-      <input type="radio" name="name" class="nav-toggle" id="item-2" >
-      <label class="nav-bottom-item" for="item-2" onclick="goto('hunhe_spf.php')">
+      <label class="nav-bottom-item" for="item-2" data-url="hunhe_spf.php">
         <i class="material-icons">trending_up</i>
         <span class="nav-bottom-text">走势</span>
       </label>
 
-      <input type="radio" name="name" class="nav-toggle" id="item-3">
-      <label class="nav-bottom-item" for="item-3" onclick="">
+      <label class="nav-bottom-item" for="item-3">
         <i class="material-icons">message</i>
         <span class="nav-bottom-text">说说</span>
       </label>
 
-      <input type="radio" name="name" class="nav-toggle" id="item-4">
-      <label class="nav-bottom-item" for="item-4" onclick="">
+      <label class="nav-bottom-item" for="item-4">
         <i class="material-icons">account_balance_wallet</i>
         <span class="nav-bottom-text">米米</span>
       </label>
 
-      <input type="radio" name="name" class="nav-toggle" id="item-5">
-      <label class="nav-bottom-item" for="item-5" onclick="goto('user.php')">
+      <label class="nav-bottom-item" for="item-5" data-url="user.php">
         <i class="material-icons">account_circle</i>
         <span class="nav-bottom-text">我的</span>
       </label>
     </div>
     <script>
-      function goto(url) {
-        if (url) {
-          window.location.href=url;
-        }
-      }
 
-      $('.nav-bottom-item').removeAttr('checked');
+      $('.nav-bottom-item').on('click',function(){
+        $('.nav-bottom-item').removeClass('active');
+        $(this).addClass('active');
+        if($(this).data('url')){window.location=$(this).data('url');}
+      })
 
       switch (window.location) {
-        case 'index.php': document.getElementById('item-1').checked='checked';
+        case 'index.php': $('.nav-bottom-item:nth-of-type(1)').addClass('active');
           break;
-        case 'hunhe_spf.php': document.getElementById('item-2').checked='checked';
+        case 'hunhe_spf.php': $('.nav-bottom-item:nth-of-type(2)').addClass('active');
           break;
-        case 'index-3.php': document.getElementById('item-3').checked='checked';
+        case 'index-3.php': $('.nav-bottom-item:nth-of-type(3)').addClass('active');
           break;
-        case 'index-4.php': document.getElementById('item-4').checked='checked';
+        case 'index-4.php': $('.nav-bottom-item:nth-of-type(4)').addClass('active');
           break;
-        case 'user.php': document.getElementById('item-5').checked='checked';
+        case 'user.php': $('.nav-bottom-item:nth-of-type(5)').addClass('active');
           break;
         default:
         break;
