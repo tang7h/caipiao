ec00075ee1da47e36a170e55c3d7ff4e83a747c6
Ruifeng Tang <tang7h@gmail.com>
2016-04-06 14:18:56 +0800

    新增瀑布流信息页面

--- bbs/index.php
+++ bbs/index.php
@@ -1,34 +1,57 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
- <html xmlns="http://www.w3.org/1999/xhtml">
- <head>
- <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
- <meta name="viewport" content="initial-scale=1">
- <title>瀑布流-Derek</title>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="initial-scale=1">
+  <meta name="format-detection" content="telephone=no" />
+  <title>瀑布流-Derek</title>
+  <link rel="stylesheet" href="../css/caipiao.css" media="screen">
+  <script type="text/javascript" src="../js/jquery-2.2.2.min.js"></script>
+  <script type="text/javascript" src="../js/waterfall.js"></script>
+</style>
+</head>
+<body>
 
- <script type="text/javascript" language="javascript" src="../js/jquery-2.2.2.min.js"></script>
- <script type="text/javascript" language="javascript" src="../js/waterfall.js"></script>
-	</style>
- </head>
- <body>
+  <div id="stage">
+    <div id="item-template">
+      <div class="item">
+        <div class="user-profile">
+          <!-- <img src="" alt="头像" /> -->
+        </div>
+        <div class="right">
+          <section class="head">
+            <div class="user-name">用户昵称</div>
+            <div class="time">发布时间</div>
+          </section>
+          <section class="content">
+            <p class="content-title"></p>
+            <div class="content-img-wrapper">
+              <img src="" alt="" class="content-img">
+            </div>
+          </section>
+          <section class="action">
+            <div class="action-item like">
+              <i class="material-icons md-18">favorite</i>
+              <span class="label-like">5</span>
+            </div>
+            <div class="action-item">
+              <i class="material-icons md-18">comment</i>
+              <span class="label-comment">7</span>
+            </div>
+          </section>
+          <section class="comments">
+            <div class="comment-item">
+              <span class="user-name">用户昵称</span>
+              <span class="comment-content">评论的内容</span>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
 
-     <div id="stage">
+  </div>
 
-       <div class="item" id="item-template">
-         <section class="head">
-           <div class="user-profile"></div>
-           <div class="user-name"></div>
-           <div class="time"></div>
-         </section>
-         <section class="content">
-           <img src=content.img alt="" class="content-img">
-           <p class="content-title"></p>
-         </section>
-         <section class="action">
+</div>
 
-         </section>
-         </div>
-       </div>
-     </div>
-
- </body>
- </html>
+</body>
+</html>
--- js/waterfall.js
+++ js/waterfall.js
@@ -1,51 +1,53 @@
 /*
- *  Javascript�ļ���waterfall.js
- */
+*  Javascript�ļ���waterfall.js
+*/
 $(function(){
-     jsonajax();
- });
+  jsonajax();
+});
 
- //������Ҫ���м�����������ǰ���ڵ�λ���ˡ����������������ײ�����100px��ʱ����Ҫ���е���ajax��������
- $(window).scroll(function(){
-     //�˷������ڹ���������ʱ�����ĺ���
-     // �����������ײ�����100����ʱ������������
-     var $doc_height,$s_top,$now_height;
-     $doc_height = $(document).height();        //������document�������߶�
-     $s_top = $(this).scrollTop();            //��ǰ����������϶��ٸ߶�
-     $now_height = $(this).height();            //������this Ҳ�Ǿ���window����
-     if(($doc_height - $s_top - $now_height) < 100) jsonajax();
- });
+//������Ҫ���м�����������ǰ���ڵ�λ���ˡ����������������ײ�����100px��ʱ����Ҫ���е���ajax��������
+$(window).scroll(function(){
+  //�˷������ڹ���������ʱ�����ĺ���
+  // �����������ײ�����100����ʱ������������
+  var $doc_height,$s_top,$now_height;
+  $doc_height = $(document).height();        //������document�������߶�
+  $s_top = $(this).scrollTop();            //��ǰ����������϶��ٸ߶�
+  $now_height = $(this).height();            //������this Ҳ�Ǿ���window����
+  if(($doc_height - $s_top - $now_height) < 100) jsonajax();
+});
 
 
- //��һ��ajax����������data.php���ϵĻ�ȡ����
- var $num = 0;
- function jsonajax(){
+//��һ��ajax����������data.php���ϵĻ�ȡ����
+var $num = 0;
+function jsonajax(){
 
-     $.ajax({
-         url:'data.php',
-         type:'POST',
-         data:"num="+$num++,
-         dataType:'json',
-         success:function(json){
-             if(typeof json == 'object'){
-                 var content,$row,iheight,temp_h;
-                 for(var i=0,l=json.length;i<l;i++){
-                     content = json[i];    //��ǰ������
-                     //���˸߶����ٵ���������������
-                     iheight  =  -1;
-                     $("#stage .item").each(function(){
-                         //�õ���ǰli�ĸ߶�
-                         temp_h = Number($(this).height());
-                         if(iheight == -1 || iheight >temp_h){
-                             iheight = temp_h;
-                             $row = $(this); //��ʱ$row��li������
-                         }
-                     });
-                     $item = $('<div><img src="../'+content.img+'" border="0" ><br/>'+content.title+'</div>').hide();
-                     $row.append($item);
-                     $item.fadeIn();
-                 }
-             }
-         }
-     });
- }
+  $.ajax({
+    url:'data.php',
+    type:'POST',
+    data:"num="+$num++,
+    dataType:'json',
+    success:function(json){
+      if(typeof json == 'object'){
+        var $content,$row,iheight,temp_h;
+        for(var i=0,l=json.length;i<l;i++){
+          $content = json[i];    //��ǰ������
+          //���˸߶����ٵ���������������
+          iheight  =  -1;
+          $("#stage").each(function(){
+            //�õ���ǰli�ĸ߶�
+            temp_h = Number($(this).height());
+            if(iheight == -1 || iheight >temp_h){
+              iheight = temp_h;
+              $row = $(this); //��ʱ$row��li������
+            }
+          });
+          $item = $($('#item-template').html()).hide();
+          $item.find('.content-img').attr('src','../'+$content.img);
+          $item.find('.content-title').html($content.title);
+          $row.append($item);
+          $item.fadeIn();
+        }
+      }
+    }
+  });
+}
