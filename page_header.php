8fb636311b1e3401bde6f8871a33c7e4a4b8f49d
Ruifeng Tang <tang7h@gmail.com>
2016-04-06 15:26:31 +0800

    修改底部导航链接地址

--- page_header.php
+++ page_header.php
@@ -2,17 +2,17 @@
   <div class="section-bottom">
     <div class="nav-bottom" id="nav-item-1">
 
-      <label class="nav-bottom-item active" for="item-1" data-url="index.php">
+      <label class="nav-bottom-item active" for="item-1" data-url="http://positemall.cn/index.php">
         <i class="material-icons">free_breakfast</i>
         <span class="nav-bottom-text">首页</span>
       </label>
 
-      <label class="nav-bottom-item" for="item-2" data-url="hunhe_spf.php">
+      <label class="nav-bottom-item" for="item-2" data-url="http://positemall.cn/hunhe_spf.php">
         <i class="material-icons">trending_up</i>
         <span class="nav-bottom-text">走势</span>
       </label>
 
-      <label class="nav-bottom-item" for="item-3">
+      <label class="nav-bottom-item" for="item-3" data-url="http://positemall.cn/bbs/index.php">
         <i class="material-icons">message</i>
         <span class="nav-bottom-text">说说</span>
       </label>
@@ -22,7 +22,7 @@
         <span class="nav-bottom-text">米米</span>
       </label>
 
-      <label class="nav-bottom-item" for="item-5" data-url="user.php">
+      <label class="nav-bottom-item" for="item-5" data-url="http://positemall.cn/member.php">
         <i class="material-icons">account_circle</i>
         <span class="nav-bottom-text">我的</span>
       </label>
@@ -35,16 +35,16 @@
         if($(this).data('url')){window.location=$(this).data('url');}
       })
 
-      switch (window.location) {
-        case 'index.php': $('.nav-bottom-item:nth-of-type(1)').addClass('active');
+      switch (window.location.pathname) {
+        case '/index.php': $('.nav-bottom-item:nth-of-type(1)').addClass('active');
           break;
-        case 'hunhe_spf.php': $('.nav-bottom-item:nth-of-type(2)').addClass('active');
+        case '/hunhe_spf.php': $('.nav-bottom-item:nth-of-type(2)').addClass('active');
           break;
-        case 'index-3.php': $('.nav-bottom-item:nth-of-type(3)').addClass('active');
+        case '/bbs/index.php': $('.nav-bottom-item:nth-of-type(3)').addClass('active');
           break;
         case 'index-4.php': $('.nav-bottom-item:nth-of-type(4)').addClass('active');
           break;
-        case 'user.php': $('.nav-bottom-item:nth-of-type(5)').addClass('active');
+        case '/member.php': $('.nav-bottom-item:nth-of-type(5)').addClass('active');
           break;
         default:
         break;
