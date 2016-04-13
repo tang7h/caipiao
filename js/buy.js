12cf4ed63765fe0a60815467a42b6e776288359d
Ruifeng Tang <tang7h@gmail.com>
2016-04-13 10:04:13 +0800

    complete 限制最多选择8场比赛

--- js/buy.js
+++ js/buy.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
   $('.cell').click(function(){
     $(this).toggleClass('mark');
-    console.log(selectionCalc(oData.data));
+    if(updateData()>8){
+      $(this).removeClass('mark');
+    }
+    console.log(updateData()+' games selected');
+    selectionCalc(oData.data);
   });
 
   var oData = new Object();
@@ -11,6 +15,7 @@ $(document).ready(function(){
 
   function updateData(){
     items = $('.range_match');
+    var games = 0;
     for(i=0; i<items.length; i++){
       gameId = $(items[i]).find('.id').html();
       selection = [];
@@ -18,25 +23,32 @@ $(document).ready(function(){
       for(j=0; j<cells.length; j++){
         selection[j]=$(cells[j]).hasClass('mark')?1:0;
       }
+      for(j=0; j<cells.length; j++){
+        if($(cells[j]).hasClass('mark')){
+          games++;
+          break;
+        }
+      }
       oData.data[i]={
         'gameId' : gameId,
         'selection' : selection
       }
     }
+    return games;
     console.log(JSON.stringify(oData));
   }
 
   function selectionCalc(data){
-    updateData();
-    var marked = 1;
+    var lottery = 1;//初始化只有一种情况
     for(i=0; i<data.length; i++){
       var count=0;
       for(var j=0; j<6; j++){
-        if(data[i].selection[j]==1)count++;
+        if(data[i].selection[j]==1)count++;//本场选中的数量
       }
-      marked *= count<1?1:count;
+      lottery *= count<1?1:count;//排列组合计算
     }
-    return marked;
+    return lottery;//返回注数
   }
+
   console.log(selectionCalc(oData.data));
 })
