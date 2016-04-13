70273a3a925410d4a695d42f8af3d5b2a4ad46f2
Ruifeng Tang <tang7h@gmail.com>
2016-04-13 14:52:52 +0800

    文件整理

--- football/hunhe_spf.php
+++ football/hunhe_spf.php
@@ -20,92 +20,91 @@ include ('../config.php');
 </head>
 <body ng-app="buyApp" ng-controller="buyCtrl">
 	<form action="" method="post">
-	<?php
-	$sql="SELECT * FROM `jczq` ORDER BY official_date,id ASC";
-			  //执行查询 得到查询结果数据集
-	$result = mysql_query($sql,$conn);
-	while($row = mysql_fetch_array($result)){
-		$current_rqspf = explode("|", $row['current_rqspf']);
-		foreach ($current_rqspf as $value) {
-		}
-		//$row['current_rqspf'] = explode('|', $row['current_rqspf']);
-		//print_r($current_rqspf); die;
-		?>
-		<div class="range_match">
-	<div class="match_id">
-		<span class="id" style="display:none;"><?php echo $row['id'];?></span><!--id-->
-		<span class="match_num"><?php echo $row['official_num'];?></span><!--official_date-->
-		<span class="match_name"><?php echo $row['match_name']?></span><!--match_name-->
-		<span class="match_date">截期时间：<?php  echo $row['official_date']?></span><!--match_date-->
-	</div>
-	<div class="match_vs">
-		<span class="home_team"><?php echo  $row['home_team']?></span><!--home_team-->
-		<span class="vs_team">VS</span>
-		<span class="away_team"><?php echo  $row['away_team']?></span><!--away_team-->
-	</div>
-	<div class="match_on">
-		<div class="match_handicap">
-			<div class="handicap_no">非让球</div>
-			<div class="handicap_yes">让球</div>
-		</div>
+		<?php
+		$sql="SELECT * FROM `jczq` ORDER BY official_date,id ASC";
+		//执行查询 得到查询结果数据集
+		$result = mysql_query($sql,$conn);
+		while($row = mysql_fetch_array($result)){
+			$current_rqspf = explode("|", $row['current_rqspf']);
+			foreach ($current_rqspf as $value) {
+			}
+			//$row['current_rqspf'] = explode('|', $row['current_rqspf']);
+			//print_r($current_rqspf); die;
+			?>
+			<div class="range_match">
+				<div class="match_id" data-game-id="<?php echo $row['id'];?>">
+					<span class="match_num"><?php echo $row['official_num'];?></span><!--official_date-->
+					<span class="match_name"><?php echo $row['match_name']?></span><!--match_name-->
+					<span class="match_date">截期时间：<?php  echo $row['official_date']?></span><!--match_date-->
+				</div>
+				<div class="match_vs">
+					<span class="home_team"><?php echo  $row['home_team']?></span><!--home_team-->
+					<span class="vs_team">VS</span>
+					<span class="away_team"><?php echo  $row['away_team']?></span><!--away_team-->
+				</div>
+				<div class="match_on">
+					<div class="match_handicap">
+						<div class="handicap_no">非让球</div>
+						<div class="handicap_yes">让球</div>
+					</div>
 
-		<div class="changci_header">
-			<div class="row">
-						<div class="cell">
-							<p>胜</p>
-							<p><?php echo  $row['current3']?></p>
+					<div class="changci_header">
+						<div class="row">
+							<div class="cell">
+								<p>胜</p>
+								<p><?php echo  $row['current3']?></p>
+							</div>
+							<div class="cell">
+								<p>平</p>
+								<p><?php echo  $row['current1']?></p>
+							</div>
+							<div class="cell">
+								<p>负</p>
+								<p><?php echo  $row['current0']?></p>
+							</div>
 						</div>
-						<div class="cell">
-							<p>平</p>
-							<p><?php echo  $row['current1']?></p>
+						<div class="row">
+							<div class="cell">
+								<p>让胜</p>
+								<p><?php echo  $current_rqspf['2']?><span>(<?php echo  $row['handicap']?>)</span></p>
+							</div>
+							<div class="cell">
+								<p>让平</p>
+								<p><?php echo  $current_rqspf['1']?><span>(<?php echo  $row['handicap']?>)</span></p>
+							</div>
+							<div class="cell">
+								<p>让负</p>
+								<p><?php echo  $current_rqspf['0']?><span>(<?php echo  $row['handicap']?>)</span></p>
+							</div>
 						</div>
-						<div class="cell">
-							<p>负</p>
-							<p><?php echo  $row['current0']?></p>
+					</div>
+					<div class="match_odds">
+						<a href="#openModal">更多</a>
+						<div id="openModal" class="match_modalDialog">
+							<div>
+								<a href="#close" title="Close" class="match_close">X</a>
+								<h2>Modal Box</h2>
+								<p>This is a sample modal box created purely in HTML and CSS.</p>
+								<p>There are many use cases for this modal box.</p>
+							</div>
 						</div>
-			</div>
-			<div class="row">
-						<div class="cell">
-							<p>让胜</p>
-							<p><?php echo  $current_rqspf['2']?><span>(<?php echo  $row['handicap']?>)</span></p>
-						</div>
-						<div class="cell">
-							<p>让平</p>
-							<p><?php echo  $current_rqspf['1']?><span>(<?php echo  $row['handicap']?>)</span></p>
-						</div>
-						<div class="cell">
-							<p>让负</p>
-							<p><?php echo  $current_rqspf['0']?><span>(<?php echo  $row['handicap']?>)</span></p>
-						</div>
-			</div>
-		</div>
-		<div class="match_odds">
-				<a href="#openModal">更多</a>
-				<div id="openModal" class="match_modalDialog">
-					<div>
-						<a href="#close" title="Close" class="match_close">X</a>
-						<h2>Modal Box</h2>
-						<p>This is a sample modal box created purely in HTML and CSS.</p>
-						<p>There are many use cases for this modal box.</p>
 					</div>
 				</div>
-		</div>
-	</div>
-</div>
-<?php
-	}
+			</div>
+			<?php
+		}
 
-	?>
+		?>
 	</form>
 
-<section id="toolbar-buy">
-	<i class="material-icons" id="btn-trolly-clean">delete</i>
-	<input type="button" name="name" value="串">
-	<div class="desc">
-		<span id="lotteries-count"></span>
-	</div>
-	<button type="button" name="button" class="md-btn md-btn-primary">选好了</button>
-</section>
-<div class="container" ng-include="'../page_header.php'"></div>
+	<section id="toolbar-buy">
+		<i class="material-icons" id="btn-trolly-clean">delete</i>
+		<input type="button" name="name" value="串">
+		<div class="desc">
+			<span id="lotteries-count"></span>
+		</div>
+		<button type="button" name="button" class="md-btn md-btn-primary">选好了</button>
+	</section>
+	<div class="container" ng-include="'../page_header.php'"></div>
 </body>
 </html>
--- js/buy.js
+++ js/buy.js
@@ -8,7 +8,6 @@ $(document).ready(function(){
     }
     $('#lotteries-count').html(oLottery.nLottery +'注 共'+oLottery.nLottery*2+'元');
     oLottery.update();
-
   });
 
   $('#btn-trolly-clean').click(function(){
@@ -17,92 +16,59 @@ $(document).ready(function(){
     $('#lotteries-count').html(oLottery.nLottery +'注 共'+oLottery.nLottery*2+'元');
   })
 
-  var oLottery = new Object();
-  oLottery.id = 123456;
-  oLottery.time = new Date();
-  oLottery.nLottery = 0;
-  oLottery.nGames = 0;
-  oLottery.data = [];
-  oLottery.update = function(){
-    var items = $('.range_match');
-    var games = 0;
-    for(i=0; i<items.length; i++){
-      gameId = $(items[i]).find('.id').html();
-      selection = [];
-      cells = $(items[i]).find('.cell');
-      for(j=0; j<cells.length; j++){
-        selection[j]=$(cells[j]).hasClass('mark')?1:0;
-      }
-      for(j=0; j<cells.length; j++){
-        if($(cells[j]).hasClass('mark')){
-          games++;
-          break;
+  var oLottery = new Object({
+    id : 123456,
+    time : new Date(),
+    nLottery : 0,
+    nGames : 0,
+    data : [],
+    update : function(){
+      var items = $('.range_match');
+      var games = 0;
+      for(i=0; i<items.length; i++){
+        gameId = $(items[i]).data('game-id');
+        selection = [];
+        cells = $(items[i]).find('.cell');
+        for(j=0; j<cells.length; j++){
+          selection[j]=$(cells[j]).hasClass('mark')?1:0;
+        }
+        for(j=0; j<cells.length; j++){
+          if($(cells[j]).hasClass('mark')){
+            games++;
+            break;
+          }
+        }
+        this.nGames = games;
+        this.data[i]={
+          'gameId' : gameId,
+          'selection' : selection
         }
       }
-      this.nGames = games;
-      this.data[i]={
-        'gameId' : gameId,
-        'selection' : selection
-      }
-    }
-    this.fCount();
-    console.log(JSON.stringify(this));
-    console.log(this.nLottery+'注');
+      this.fCount();
+      localStorage.oLottery = JSON.stringify(this);
+      // console.log(JSON.stringify(this));
+      // console.log(this.nLottery+'注');
 
-  }
-  oLottery.fCount = function(){
-    var lottery = 0;//初始化只有一种情况
-    if(this.nGames>0){
-      lottery = 1;
-    }
-    for(i=0; i<this.data.length; i++){
-      var count=0;
-      for(var j=0; j<6; j++){
-        if(this.data[i].selection[j]==1)count++;//本场选中的数量
+    },
+    fCount : function(){
+      var lottery = 0;//初始化只有一种情况
+      if(this.nGames>0){
+        lottery = 1;
       }
-      if(count>0){
-        lottery *= count;//排列组合计算
-      }
-    }
-    this.nLottery = lottery;
-  }
-
-  function updateData(){
-    items = $('.range_match');
-    var games = 0;
-    for(i=0; i<items.length; i++){
-      gameId = $(items[i]).find('.id').html();
-      selection = [];
-      cells = $(items[i]).find('.cell');
-      for(j=0; j<cells.length; j++){
-        selection[j]=$(cells[j]).hasClass('mark')?1:0;
-      }
-      for(j=0; j<cells.length; j++){
-        if($(cells[j]).hasClass('mark')){
-          games++;
-          break;
+      for(i=0; i<this.data.length; i++){
+        var count=0;
+        for(var j=0; j<6; j++){
+          if(this.data[i].selection[j]==1)count++;//本场选中的数量
+        }
+        if(count>0){
+          lottery *= count;//排列组合计算
         }
       }
-      oLottery.data[i]={
-        'gameId' : gameId,
-        'selection' : selection
-      }
+      this.nLottery = lottery;
     }
-    console.log(JSON.stringify(oLottery));
-    return games;//返回数据：选中的场次数量
-  }
 
-  function selectionCount(data){
-    var lottery = 1;//初始化只有一种情况
-    for(i=0; i<data.length; i++){
-      var count=0;
-      for(var j=0; j<6; j++){
-        if(data[i].selection[j]==1)count++;//本场选中的数量
-      }
-      lottery *= count<1?1:count;//排列组合计算
-    }
-    return lottery;//返回注数
-  }
+  });
+
 
   // angular
   var buyApp = angular.module('buyApp',[]);
